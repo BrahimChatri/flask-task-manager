75baf44dafd3e29cca180469d6f82a8a12baf37e
Brahim Chatri <brahim.chatri.dev@gmail.com>
2025-04-08 02:57:58 +0100

    remove logs file

--- utils/logger.py
+++ utils/logger.py
@@ -5,7 +5,7 @@ logging_format = logging.Formatter("[%(asctime)s | %(name)s] %(message)s")
 second_format = logging.Formatter("-> %(message)s")
 
 # Log handler for writing logs to /tmp/logs.log (Vercel allows writing to /tmp)
-log_handler = logging.FileHandler("/tmp/logs.log")
+
 
 # Stream handler to output logs to console (visible in Vercel logs)
 stream_handler = logging.StreamHandler()
@@ -14,11 +14,10 @@ stream_handler.setFormatter(second_format)
 # Setting up the Info logger
 Info_logger = logging.getLogger("INFO")
 Info_logger.setLevel(logging.INFO)  # INFO level is 20
-Info_logger.addHandler(log_handler)
+
 Info_logger.addHandler(stream_handler)
 
 # Setting up the Error logger
 Error_logger = logging.getLogger("ERROR")
 Error_logger.setLevel(logging.ERROR)  # ERROR level is 40
-Error_logger.addHandler(log_handler)
 Error_logger.addHandler(stream_handler)
