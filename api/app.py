f5839277ff47485ce34f29b356f8c0d33f444215
Brahim Chatri <brahim.chatri.dev@gmail.com>
2025-04-08 02:45:20 +0100

    Prepare for deployment

---
+++ api/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from utils.views import views
+from dotenv import load_dotenv
+import os
+from datetime import timedelta
+
+load_dotenv()
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.register_blueprint(views, url_prefix='/')
+
+app.config['SECRET_KEY'] = os.getenv("KEY")
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html"), 404
+
+# Vercel requires this as handler
+handler = app
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
